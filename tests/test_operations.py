60c969f4b78281dcec632b593741bdc909e7f2a7
kaw393939 <keith@firehose360.com>
2025-09-15 19:31:26 -0400

    refactored addition

---
+++ tests/test_operations.py
@@ -0,0 +1,5 @@
+from src.operations import addition
+
+
+def test_addition():
+    assert addition(2, 3) == 5
--- tests/test_sanity.py
+++
@@ -1,5 +0,0 @@
-from src.main import addition
-
-
-def test_sanity():
-    assert addition(2, 3) == 5
