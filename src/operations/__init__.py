bad4f0f0a437c4cfe991c66d5674f73f17a55654
kaw393939 <keith@firehose360.com>
2025-09-15 19:33:49 -0400

    added subtraction

--- src/operations/__init__.py
+++ src/operations/__init__.py
@@ -1,2 +1,6 @@
 def addition(a, b):
     return a + b
+
+def subtraction(a, b):
+    return a - b
+
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,5 +1,7 @@
-from src.operations import addition
-
+from src.operations import addition, subtraction
 
 def test_addition():
     assert addition(2, 3) == 5
+
+def test_subtraction():
+    assert subtraction(5, 3) == 2
